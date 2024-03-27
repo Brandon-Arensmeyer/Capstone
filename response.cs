55fe77e77d7bdc7691ee08da653b38032f649062
Brandon Arensmeyer <b73x527@gfcmsu.edu>
2024-03-27 16:39:49 -0600

    websocket

--- response.cs
+++ response.cs
@@ -22,6 +22,20 @@ using System.Threading.Tasks;
 using System.Text.Json.Nodes;
 using System.Security.Cryptography.X509Certificates;
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Net.WebSockets;
+
+
+
+
 
 var openAI = new OpenAIClient("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");
 string readResource (string resourceName) {
@@ -55,7 +69,7 @@ System.BinaryData makeParameters(params (string, string, string)[] parameters) {
     return BinaryData.FromObjectAsJson(o);
 }
 
-Task<StreamingResponse<StreamingChatCompletionsUpdate>> getCompletion(List<ChatRequestMessage> context,String prompt) {
+Task<StreamingResponse<StreamingChatCompletionsUpdate>> getStreamingCompletion(List<ChatRequestMessage> context,String prompt) {
     var options = new ChatCompletionsOptions();
     options.DeploymentName = "gpt-4-1106-preview";
     options.Tools.Add(
@@ -123,6 +137,74 @@ Task<StreamingResponse<StreamingChatCompletionsUpdate>> getCompletion(List<ChatR
     return openAI.GetChatCompletionsStreamingAsync(options);
 }
 
+Task<Response<ChatCompletions>> getCompletion(List<ChatRequestMessage> context,String prompt) {
+    var options = new ChatCompletionsOptions();
+    options.DeploymentName = "gpt-4-1106-preview";
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "getWeather", 
+                Description = "You will recive the weather and the JSON document that describes the clouds, temperature, humidity, etc...",
+                Parameters = makeParameters(
+                    ("cityName", "string", "name of the city we want the weather for")
+                )}));
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "getQuote", 
+                Description = "You will recive a quote and the JSON document that gives the author of the quote, its tags, and its id.",
+                Parameters = makeParameters(
+                    ("quote", "string", "the type of quote that we want")
+                )}));
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "getJoke", 
+                Description = "You will recive a chuck norris joke",
+                Parameters = makeParameters(
+                    ("joke", "string", "the type of joke that we want")
+                )}));
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "NFLSchedule", 
+                Description = "To the weekly schedule of the NFL schedule, should contain all the information about the teams who are playing that week",
+                Parameters = makeParameters(
+                    ("Year", "number", "the start of the year as an integer of the NFL schedule you are asking for"),
+                    ("Week", "number", "the week as an integer number of the schedule, with week number one being the start of the season")
+                )}));
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "NFLRoster", 
+                Description = "It gives you the players on a team durring a specific season",
+                Parameters = makeParameters(
+                    // ("Year", "number", "the start of the year as an integer of the NFL schedule you are asking for"),
+                    ("team_name", "number", "You will be given the team name and convert it to the team id number based on the list in the system prompt. Once you get the team id, you will put that id into the url where {team_name} is located")
+                )}));
+    options.Tools.Add(
+        new ChatCompletionsFunctionToolDefinition(
+            new FunctionDefinition(){
+                Name = "NFLRecord", 
+                Description = "It will give you the wins, losses and ties in their season",
+                Parameters = makeParameters(
+                    // ("Year", "number", "the start of the year as an integer of the NFL schedule you are asking for"),
+                    ("team_name", "number", "You will be given the team name and convert it to the team id number based on the list in the system prompt. Once you get the team id, you will put that id into the url where {team_name} is located")
+                )}));
+    if(context.Count == 0){
+        context.Add(new ChatRequestSystemMessage($"{systemFormatingPrompt}\n You are an AI. "));
+    }
+    if(!String.IsNullOrWhiteSpace(prompt)){
+        context.Add(new ChatRequestUserMessage(prompt));
+
+    }
+    
+    foreach(var m in context){
+        options.Messages.Add(m);
+    }
+    return openAI.GetChatCompletionsAsync(options);
+}
+
 Func<LiveDisplayContext, Task> updateLayoutAsync(string prompt){
     var context = new List<ChatRequestMessage>();
     return async ldc =>{
@@ -160,7 +242,7 @@ Func<LiveDisplayContext, Task> updateLayoutAsync(string prompt){
             string? toolCallId = null;
             string functionArgs = "";
 
-            var updates = await getCompletion(context, prompt);
+            var updates = await getStreamingCompletion(context, prompt);
             await foreach(var chunk in updates){
                 if (chunk.Role is not null) {
                     role = chunk.Role;
@@ -273,7 +355,124 @@ Func<LiveDisplayContext, Task> updateLayoutAsync(string prompt){
     
 }
 
+async Task<string> processPrompt(List<ChatRequestMessage> context, String prompt){
+        var txtString = "";
+        var cont = false;
+        var counter = 0;
+        do{
+            ChatRole? role = null;
+            string? functionName = null;
+            string? toolCallId = null;
+            string functionArgs = "";
 
+            var updates = await getCompletion(context, prompt);
+            var message = updates.Value.Choices[0].Message;
+
+            
+            
+            role = message.Role;    
+            switch (role){
+                case var r when r == ChatRole.Assistant:
+                    if (message.ToolCalls.Count == 1 && message.ToolCalls[0] is ChatCompletionsFunctionToolCall ftc) {
+                        toolCallId = ftc.Id;
+                        functionName = ftc.Name;
+                        
+                        functionArgs = ftc.Arguments;
+                        txtString += $"{functionName}\n{functionArgs}";
+                    }else{
+                        txtString += message.Content;
+
+                    }
+                    break;
+                
+            }
+                
+            if(String.IsNullOrEmpty(functionName)){
+                context.Add(new ChatRequestAssistantMessage(message.Content));
+                cont = false;
+            }
+            else{
+    
+
+
+                if(functionName == "getQuote"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);
+                    var cityName = parameters["quote"];
+                    while(counter == 0){
+                        context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await quote()}}"""));
+                        cont = true;
+                        // add counter
+                        // txtString += $"\ncityName: {cityName}\n";
+                        prompt = "";                            
+                        counter += 1;
+                    }
+                        
+                }
+                else if(functionName == "getWeather"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);
+                    var cityName = parameters["cityName"];
+                            
+                    context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await GetWeatherData(cityName)}}"""));
+                            
+                    cont = true;
+                    // add counter
+                    // txtString += $"\ncityName: {cityName}\n";
+                    prompt = "";
+                    counter += 1;    
+                }
+                else if(functionName == "getJoke"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);
+                    var cityName = parameters["joke"];
+                            
+                    context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await joke()}}"""));
+                            
+                    cont = true;
+                    // add counter
+                    // txtString += $"\ncityName: {cityName}\n";
+                    prompt = "";
+                    counter += 1;    
+                }
+                else if(functionName == "NFLSchedule"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);
+                    var year = Int32.Parse(parameters["Year"].ToString());
+                    var week = Int32.Parse(parameters["Week"].ToString());
+                            
+                    context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await NFLSchedule((int) year,(int) week)}}"""));
+                            
+                    cont = true;
+                    // add counter
+                    // txtString += $"\ncityName: {cityName}\n";
+                    prompt = "";
+                    counter += 1;    
+                }
+                else if(functionName == "NFLRoster"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);        
+                    // var year = Int32.Parse(parameters["Year"].ToString());
+                    var ID = Int32.Parse(parameters["team_name"].ToString());
+                    context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await NFLRoster((int) ID)}}"""));
+                            
+                    cont = true;
+                    // add counter
+                    // txtString += $"\ncityName: {cityName}\n";
+                    prompt = "";
+                    counter += 1;    
+                }
+                else if(functionName == "NFLRecord"){
+                    var parameters = System.Text.Json.JsonSerializer.Deserialize<System.Collections.Generic.Dictionary<string, object>>(functionArgs);  
+                    // var year = Int32.Parse(parameters["Year"].ToString());
+                    var team_name = parameters["team_name"].ToString();      
+                    context.Add(new ChatRequestFunctionMessage(functionName, $$"""{{{await NFLRecord((string) team_name)}}"""));
+                            
+                    cont = true;
+                    prompt = "";
+                    counter += 1;    
+                }
+            }
+                
+            
+        }while(cont);
+    return txtString;
+}
 
 static async Task Weather()
     {
@@ -500,34 +699,123 @@ await AnsiConsole.Progress()
 
 // This creates to simatanious load up sequencens
 
-AnsiConsole.Status()
-    .Start("One second...", ctx => 
-    {
-        // Update the status and spinner
-        ctx.Status("One second...");
-        ctx.Spinner(Spinner.Known.Christmas);
-        ctx.SpinnerStyle(Style.Parse("green"));
+// AnsiConsole.Status()
+//     .Start("One second...", ctx => 
+//     {
+//         // Update the status and spinner
+//         ctx.Status("One second...");
+//         ctx.Spinner(Spinner.Known.Christmas);
+//         ctx.SpinnerStyle(Style.Parse("green"));
         
-        // Simulate some work
-        AnsiConsole.MarkupLine("Getting some coffee...");
-        Thread.Sleep(0);
+//         // Simulate some work
+//         AnsiConsole.MarkupLine("Getting some coffee...");
+//         Thread.Sleep(0);
         
        
 
-        // Simulate some work
-        AnsiConsole.MarkupLine("Stretching...");
-        Thread.Sleep(0);
+//         // Simulate some work
+//         AnsiConsole.MarkupLine("Stretching...");
+//         Thread.Sleep(0);
+//     });
+
+
+void startup(){
+    var builder = WebApplication.CreateBuilder(Environment.GetCommandLineArgs());
+    var app = builder.Build();
+    app
+        .UseDeveloperExceptionPage()
+        .UseHttpsRedirection()
+        .UseDefaultFiles()
+        .UseStaticFiles()
+        .UseRouting()
+        .UseWebSockets();
+
+    var counter = 0;
+    var runCounter = false;
+
+    async Task ProcessWs(WebSocket webSocket){
+        var buffer = new byte[1024 * 4];
+        var rcvBuffer = new byte[1024 * 4];
+        var contRcv = true;
+
+        async Task RcLoop(){
+            while(contRcv){
+                Console.WriteLine("rcvLoop waiting");
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(rcvBuffer), CancellationToken.None);
+                Console.WriteLine($@"Ws:Close Status: {result.CloseStatus}
+                                    Type: {result.MessageType}
+                                    Count:{result.Count}
+                                    EndOfMessage: {result.EndOfMessage}
+                                    Content: {Encoding.UTF8.GetString(rcvBuffer, 0, result.Count)}");
+            }
+        }
+        async Task Loop(){
+            if (runCounter){
+                var message = $"<div id=\"counter\" hx-swap-oob=\"true\">{counter}</div>";
+                if (webSocket.CloseStatus.HasValue){
+                    Console.WriteLine("Ending websocket connection ...");
+                }
+                else{
+                    var len = Encoding.UTF8.GetBytes(message, 0, message.Length, buffer, 0);
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, len), WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+                counter++;
+            }
+            await Task.Delay(1000);
+            await Loop();
+        }
+
+        var rcvFinished = RcLoop();
+        await Loop();
+        Console.WriteLine("stopping receive message loop");
+        contRcv = false;
+        await rcvFinished;
+    }
+    
+    app.MapGet("/bob", async (HttpContext context) =>
+    {
+        runCounter = true;
+        await context.Response.WriteAsync($@"<button hx-get=""/stop"" hx-swap=""outerHTML"">Stop Counter</button>");
+    });
+    app.MapGet("/stop", async (HttpContext context) => {
+        runCounter = false;
+        await context.Response.WriteAsync($@"<button hx-get=""/bob"" hx-swap=""outerHTML"">Start Counter</button>");
+    });
+    app.Use((context, next) => {
+        if(context.Request.Path == "/prompt"){
+            Console.WriteLine("connecting with a websocket");
+            var webSocket = context.WebSockets.AcceptWebSocketAsync().Result;
+            return ProcessWs(webSocket);
+        }
+        else{
+            return next.Invoke();
+        }
     });
 
+    app.Run();
 
-
+    // app.MapGet("/bob",()=>{
+    //     return """<button hx-get="/bill" hx-swap="outerHTML" >Goodbye</button>""";
+    // });
+    // app.MapGet("/bill",()=>{
+    //     return """<p style="color:red" >Goodbye</p>""";
+    // });
+    // app.MapPost("/prompt", async (Dictionary<String, Object> o)=>{
+    //     Console.WriteLine($"{o["Work"]}");
+    //     return await processPrompt(new List<ChatRequestMessage>(), o["Work"].ToString());
+        
+    // });
+    // app.Run("https://localhost:5000");
+}
 
 // This is where you can use all the programs created above
+startup();
 
-AnsiConsole.MarkupLine("[yellow]Welcome, I am your chatbot![/]");
-var cts = new CancellationTokenSource();
-while(true){
-    var prompt = await(new TextPrompt<string>("> ")).ShowAsync(AnsiConsole.Console,cts.Token);
-    await AnsiConsole.Live((new Panel(new Markup("Hello"))).Expand().Header("Response", Justify.Center)).StartAsync(updateLayoutAsync(prompt));
-}
+// This is for the console if you want to run it as a console
+// AnsiConsole.MarkupLine("[yellow]Welcome, I am your chatbot![/]");
+// var cts = new CancellationTokenSource();
+// while(true){
+//     var prompt = await(new TextPrompt<string>("> ")).ShowAsync(AnsiConsole.Console,cts.Token);
+//     await AnsiConsole.Live((new Panel(new Markup("Hello"))).Expand().Header("Response", Justify.Center)).StartAsync(updateLayoutAsync(prompt));
+// }
 
